f758d76b01713d4ef9578f790decc5d2ad2e5bc6
jennlopezdv <jenniferlopezdavid71@gmail.com>
2026-01-06 15:43:18 -0500

    Actividad finalizada

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -1,28 +1,22 @@
-/*let nombre = "Jennifer"
-console.log(nombre)*/
-
 let nombre = prompt("Ingrese su nombre: ");
 let edad = Number(prompt("Ingrese su edad: "));
 
-/*Comprobar que el valor ingresado en edad es número, sino
-*/
-if (Number.isNaN(edad)) {
-    console.error("Error: Por favor, ingresa una edad válida en números.");
+//Comprobar que el valor ingresado en edad es válido//
+if (Number.isNaN(edad) || edad < 0) {
+    console.error("Error: Por favor, ingresa una edad válida.");
 } else {
-    console.log("Edad válida.")
-}
+    //Si la edad es menor a 18//
+    if (edad < 18){
+        alert(`Hola ${nombre}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!`)}
 
+    //Si la edad es mayor o igual a 18//
+    else(edad >= 18)
+        console.log(`Hola ${nombre}, eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!`)
+    }        
 
-//Si la edad es menor a 18//
-if (edad < 18){
- alert(`Hola ${nombre}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!`)}
-
- //Si la edad es mayor o igual a 18//
- else if (edad >= 18){
-    console.log(`Hola ${nombre}, eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!`)
- }
  
- else{
-    console.log("Edad inválida.")
- }
+
+
+
+
 
